3103fb234158eb2d297d3e88fe82090a3bd30d16
Hyeonwoo Kim <allone187@naver.com>
2022-10-24 18:44:02 +0900

    8th commit

---
+++ Chap03/Chap03_2/03_2_4_condition07.py
@@ -0,0 +1,22 @@
+a = float(input("학점을 입력하시오: "))
+
+if a == 4.5:
+    print("신")
+elif 4.2 <= a:
+    print("교수님의 사랑")
+elif 3.5 <= a:
+    print("현 체재의 수호자")
+elif 2.8 <= a:
+    print("일반인")
+elif 2.3 <= a:
+    print("일탈을 꿈꾸는 소시민")
+elif 1.75 <= a:
+    print("오락문화의 선구자")
+elif 1.0 <= a:
+    print("불가촉천민")
+elif 0.5 <= a:
+    print("자벌레")
+elif 0 < a:
+    print("플랑크톤")
+else:
+    print("시대를 앞서가는 혁명의 씨앗")
