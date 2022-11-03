165d79f9b24922bbca4e73c2193c219dfa08c5b9
Hyeonwoo Kim <allone187@naver.com>
2022-11-03 23:42:03 +0900

    10th

--- Chap04/Chap04_1/04_1_3_list03.py
+++ Chap04/Chap04_1/04_1_3_list03.py
@@ -6,3 +6,18 @@ print("del list_a[1]: ", list_a)
 
 list_a.pop(2)
 print("pop(2): ", list_a)
+
+#값으로 제거하는 법: remove() -> 중복된 값 중에 첫번째부터 제거
+list_1 = [1, 2, 3, 2, 4]
+list_1.remove(2)
+print("list_1 > ", list_1)
+
+#모두 제거하는 법: clear()
+list_2 = [1, 2, 3, 4, 5]
+list_2.clear()
+print("list_2 > ", list_2)
+
+#리스트 내부에 있는지 확인하는 방법: in/not in 연산자
+list_3 = [1, 2, 3, 4, 5]
+print(1 in list_3)
+print(100 not in list_3)
---
+++ Chap04/Chap04_1/04_1_4_for_list.py
@@ -0,0 +1,11 @@
+#예제 전
+for i in range(5):
+    print(100)
+
+array = [1, 2, 3, 4, 5]
+
+for element in array:
+    print(element)
+
+for chr in "hello":
+    print("-", chr)
