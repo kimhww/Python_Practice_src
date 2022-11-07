cebd4665436bcd06b41e2fb87a10ecda68eedeed
Hyeonwoo Kim <allone187@naver.com>
2022-11-07 19:59:49 +0900

    11th

---
+++ Chap04/Chap04_2/04_2_2_dict02.py
@@ -0,0 +1,9 @@
+dic = {}
+
+print("요소 추가 전> ", dic)
+
+dic["name"] = "새로운 이름"
+dic["head"] = "새로운 정신"
+dic["body"] = "새로운 몸"
+
+print("요소 추가 후> ", dic)
---
+++ Chap04/Chap04_2/04_2_3_dict03.py
@@ -0,0 +1,11 @@
+dic = {
+    "name": "7D 망고",
+    "type": "당절임"
+}
+
+print("요소 제거 전> ", dic)
+
+del dic["name"]
+del dic["type"]
+
+print("요소 제거 후> ", dic)
